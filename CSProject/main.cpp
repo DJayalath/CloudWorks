fd16798c7db2475ab83ea58c3ea88ce61ec551c9
Dulhan Jayalath <dhjayalath@gmail.com>
2018-10-10 20:03:03 +0100

    Add project files.

---
+++ CSProject/main.cpp
@@ -0,0 +1,12 @@
+/* NOTES 
+* Using Multithreaded Debug DLL for Debug mode only to avoid
+* LNK2019 Vector error
+*/
+
+// REMOVE after Debugging
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	return 0;
+}
